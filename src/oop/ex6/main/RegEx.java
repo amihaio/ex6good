b9a72eef5b205c1501d4ce85e7d69b9f2775f5c2
sapirshabi <sapir.shabi@mail.huji.ac.il>
2016-06-01 18:29:51 +0300

    added regex

--- src/oop/ex6/main/RegEx.java
+++ src/oop/ex6/main/RegEx.java
@@ -14,5 +14,15 @@ public class RegEx {
 
     //todo illegal operators and comments
 
-
+    public static String closeScope = "^\\s*}\\s*$";
+    public static String getCloseScope = "^\\s*{\\s*$";
+    public static String varAssignment = "^\\s*(?:final\\s+)*(int|boolean|double|char|String)\\s+([A-Za-z][A-Za-z0-9]*)\\s*=\\s*([\\w]|[\\.[\\]\"{}()*+-?^$|])+\\s*;";
+    public static String varDefined= "^\\s*(?:final\\s+)*(int|boolean|double|char|String)\\s+([A-Za-z][A-Za-z0-9]*)\\s*;";
+    public static String mrthodCalling = "";
+    public static String methodSignature = "";
+    public static String intValue = "^\\d+$";
+    public static String booleanValue = "^(true|false)$";
+    public static String doubleValue = ""; // TODO
+    public static String charValue =  "^'[^';]*'$";
+    public static String stringValue = "^\"[^\";]*\"$";
 }
