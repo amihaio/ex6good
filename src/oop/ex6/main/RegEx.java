4586681ef776626f3bcadb033a160903c3d6373a
amihao <amihai.offenbacher@mail.huji.ac.il>
2016-06-02 20:10:19 +0300

    first commit

--- src/oop/ex6/main/RegEx.java
+++ src/oop/ex6/main/RegEx.java
@@ -3,26 +3,39 @@ package oop.ex6.main;
 public class RegEx {
     //make static
 
-    public String methodDec = "(?:private|public|protected)?\\s+void\\s+(\\w+)?\\s\\((\\s*" +
-            "(int|String|boolean|double|char)\\s+(\\w+){1},?)*\\)\\{\\s*";  //todo comma issue
+    public String methodDec = "(?:private|public|protected)?\\s+void\\s+([A-Za-z][A-Za-z0-9]*)?\\s*\\(\\s*" +
+            "(|(int|String|boolean|double|char)\\s+([A-Za-z][A-Za-z0-9_]*)(\\s*,\\s*" +
+            "(int|String|boolean|double|char)\\s+([A-Za-z][A-Za-z0-9]*))*)\\)\\{\\s*";
 
-    public String ifDec = "if\\s*\\(\\)\\s*{\\s*";
+    public String ifDec = "if\\s*\\(\\s*([a-zA-z][\\w]*|true|false|(-?[1-9]+.?[1-9]*))(|(\\s+(&&|\\|\\|)" +
+            "\\s+([a-zA-z][\\w]*|true|false|(-?[1-9]+.?[1-9]*)))*)\\s*\\)\\s*\\{";
 
     public String comment = "^\\/\\/";
 
-    public String assignment = "\\^\\s*\\w+\\s+={1}\\s+";
+    public String varAssignment = "\\^\\s*\\w+\\s+={1}\\s+";
 
     //todo illegal operators and comments
+    //todo var names cannot start with digit
 
     public static String closeScope = "^\\s*}\\s*$";
-    public static String getCloseScope = "^\\s*{\\s*$";
-    public static String varAssignment = "^\\s*(?:final\\s+)*(int|boolean|double|char|String)\\s+([A-Za-z][A-Za-z0-9]*)\\s*=\\s*([\\w]|[\\.[\\]\"{}()*+-?^$|])+\\s*;";
-    public static String varDefined= "^\\s*(?:final\\s+)*(int|boolean|double|char|String)\\s+([A-Za-z][A-Za-z0-9]*)\\s*;";
-    public static String mrthodCalling = "";
-    public static String methodSignature = "";
+
+    public static String varDec = "^\\s*(?:final\\s+)*(int|boolean|double|char|String)\\s+" +
+            "([A-Za-z][A-Za-z0-9]*)\\s*=\\s*([\\w]|[\\.[\\]\"{}()*+-?^$|])+\\s*;";
+
+    public static String varDefined= "^\\s*(?:final\\s+)*(int|boolean|double|char|String)\\s+" +
+            "([A-Za-z][A-Za-z0-9]*)\\s*;";
+
+    public static String methodCalling = "([a-zA-z][\\w]*)\\s*\\(\\s*(|(((int|boolean|double|char|String)" +
+            "\\s+[a-zA-z][\\w]*)(\\s*,\\s*(int|boolean|double|char|String)\\s+[a-zA-z][\\w]*)*))\\s*\\)\\s*;";
+
     public static String intValue = "^\\d+$";
-    public static String booleanValue = "^(true|false)$";
-    public static String doubleValue = ""; // TODO
+
+    public static String booleanValue = "^(true|false|-?[1-9]+.?[1-9]*)$";
+
+    public static String doubleValue = "^-?[1-9]+.?[1-9]*$";
+
     public static String charValue =  "^'[^';]*'$";
+
     public static String stringValue = "^\"[^\";]*\"$";
+
 }
--- src/oop/ex6/sJavaobjects/InnerBooleanBlock.java
+++ src/oop/ex6/sJavaobjects/InnerBooleanBlock.java
@@ -1,5 +1,25 @@
 package oop.ex6.sJavaobjects;
 
 
+import java.util.ArrayList;
+
 public class InnerBooleanBlock {
+    private ArrayList<VariableObject> globalVariables;
+    private ArrayList<MethodObject> methods;
+    private ArrayList<VariableObject> localVariables;
+    private ArrayList<String> blockLines;
+
+    public InnerBooleanBlock(ArrayList<VariableObject> globalVariables, ArrayList<MethodObject> methods,
+                             ArrayList<String> blockLines) {
+        this.globalVariables = globalVariables;
+        this.methods = methods;
+        this.blockLines = blockLines;
+    }
+
+    private void validate() {
+        for (String line : blockLines) {
+
+        }
+    }
+
 }
